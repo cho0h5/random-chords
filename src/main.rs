b1ccd1ca84199e3003920dfa861d8f8536a924d2
Youngho Cho <dominoyh5@gmail.com>
2020-07-15 17:41:05 +0900

    Update src/main.rs to Input BPM

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+//use std::{thread, time};
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut bpm = 0;
+    loop {
+        println!("Enter bpm: ");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        match input.trim().parse() {
+            Ok(num) => {
+                bpm = num;
+                break;
+            },
+            Err(err) => println!("{}", err),
+        };
+    }
+
+    println!("{}", bpm);
+    //thread::sleep(time::Duration::from_millis(500));
 }
